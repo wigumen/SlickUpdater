0f61882673da07f3e327b083e38d6306ca0919b9
Finn-TheHuman <franklin.5689@gmail.com>
2014-03-19 13:33:53 -0700

    Re Init
    Commit to a un broken version

---
+++ SlickUpdater/Arma3LaunchOptionsDialogue.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SlickUpdater {
+    /// <summary>
+    /// Interaction logic for Arma3LaunchOptionsDialogue.xaml
+    /// </summary>
+    public partial class Arma3LaunchOptionsDialogue : Window {
+        public Arma3LaunchOptionsDialogue() {
+            InitializeComponent();
+            initializeValues();
+        }
+        void initializeValues() {
+
+            if (ConfigManager.fetch("ArmA3", "window") == "true") {
+                window.IsChecked = true;
+            } else {
+                window.IsChecked = false;
+            }
+
+            if (ConfigManager.fetch("ArmA3", "nosplash") == "true") {
+                nosplash.IsChecked = true;
+            } else {
+                nosplash.IsChecked = false;
+            }
+
+            if (ConfigManager.fetch("ArmA3", "skipIntro") == "true") {
+                skipIntro.IsChecked = true;
+            } else {
+                skipIntro.IsChecked = false;
+            }
+
+            if (ConfigManager.fetch("ArmA3", "noLogs") == "true") {
+                noLogs.IsChecked = true;
+            } else {
+                noLogs.IsChecked = false;
+            }
+
+            if (ConfigManager.fetch("ArmA3", "noPause") == "true") {
+                noPause.IsChecked = true;
+            } else {
+                noPause.IsChecked = false;
+            }
+
+            if (ConfigManager.fetch("ArmA3", "showScriptErrors") == "true") {
+                showScriptErrors.IsChecked = true;
+            } else {
+                showScriptErrors.IsChecked = false;
+            }
+
+            world.Text = ConfigManager.fetch("ArmA3", "world");
+            customParams.Text = ConfigManager.fetch("ArmA3", "customParameters");
+        }
+
+        private void window_Click(object sender, RoutedEventArgs e) {
+            if (window.IsChecked == true) {
+                ConfigManager.write("ArmA3", "window", "true");
+            } else {
+                ConfigManager.write("ArmA3", "window", "false");
+            }
+        }
+
+        private void nosplash_Click(object sender, RoutedEventArgs e) {
+            if (nosplash.IsChecked == true) {
+                ConfigManager.write("ArmA3", "nosplash", "true");
+            } else {
+                ConfigManager.write("ArmA3", "nosplash", "false");
+            }
+        }
+
+        private void skipIntro_Click(object sender, RoutedEventArgs e) {
+            if (skipIntro.IsChecked == true) {
+                ConfigManager.write("ArmA3", "skipIntro", "true");
+            } else {
+                ConfigManager.write("ArmA3", "skipIntro", "false");
+            }
+        }
+
+        private void noLogs_Click(object sender, RoutedEventArgs e) {
+            if (noLogs.IsChecked == true) {
+                ConfigManager.write("ArmA3", "noLogs", "true");
+            } else {
+                ConfigManager.write("ArmA3", "noLogs", "false");
+            }
+        }
+
+        private void noPause_Click(object sender, RoutedEventArgs e) {
+            if (noPause.IsChecked == true) {
+                ConfigManager.write("ArmA3", "noPause", "true");
+            } else {
+                ConfigManager.write("ArmA3", "noPause", "false");
+            }
+        }
+
+        private void showScriptErrors_Click(object sender, RoutedEventArgs e) {
+            if (showScriptErrors.IsChecked == true) {
+                ConfigManager.write("ArmA3", "showScriptErrors", "true");
+            } else {
+                ConfigManager.write("ArmA3", "showScriptErrors", "false");
+            }
+        }
+
+        private void world_TextChanged(object sender, TextChangedEventArgs e) {
+            ConfigManager.write("ArmA3", "world", world.Text);
+        }
+
+        private void customParams_TextChanged(object sender, TextChangedEventArgs e) {
+            ConfigManager.write("ArmA3", "customParameters", customParams.Text);
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            WindowManager.mainWindow.IsEnabled = true;
+        }
+    }
+}
---
+++ SlickUpdater/Launch.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace SlickUpdater {
+    static class Launch {
+        static public void a3Launch(bool connectToServer, string server) {
+            string arma3Path = regcheck.arma3RegCheck() + "\\arma3.exe";
+            string world = ConfigManager.fetch("ArmA3", "world");
+            string customParams = ConfigManager.fetch("ArmA3", "customParameters");
+            string mods = modlister();
+
+            string args = "";
+            if (ConfigManager.fetch("ArmA3", "window") == "true") {
+                args += " -window";
+            }
+            if (ConfigManager.fetch("ArmA3", "nosplash") == "true") {
+                args += " -nosplash";
+            }
+            if (ConfigManager.fetch("ArmA3", "skipIntro") == "true") {
+                args += " -skipIntro";
+            }
+            if (ConfigManager.fetch("ArmA3", "noLogs") == "true") {
+                args += " -noLogs";
+            }
+            if (ConfigManager.fetch("ArmA3", "noPause") == "true") {
+                args += " -noPause";
+            }
+            if (ConfigManager.fetch("ArmA3", "showScriptErrors") == "true") {
+                args += " -showScriptErrors";
+            }
+            if (connectToServer == true)
+            {
+                if (server == "PA Repo")
+                {
+                    args += " -port=2302 -connect=216.155.136.21 -password=PA";
+                }
+                else if (server == "Test Outfit Repo")
+                {
+                    args += " -port=2302 -connect=72.5.102.119 -password=scott";
+                }
+            }
+            if (world != "") {
+                if (world == "demwaffels") {
+                    System.Diagnostics.Process.Start("http://www.youtube.com/watch?v=8W5WdS7q3ns");
+                    return;
+                } else {
+                    args += " -world=" + world;
+                }
+            }
+            if (customParams != "") {
+                args += " " + customParams;
+            }
+            if (mods != "") {
+                args += " -mod=" + mods;
+            }
+
+            Process.Start(arma3Path, args);
+            logIt.addData("Launched Arma 3 with " + args);
+        }
+        //added ArmA2 OA launcher
+        static public void a2Launch(bool connectToServer, string server)
+        {
+            string arma2Path = regcheck.arma2RegCheck() + "\\ArmA2OA.exe";
+            string world = ConfigManager.fetch("ArmA2", "world");
+            string customParams = ConfigManager.fetch("ArmA2", "customParameters");
+            string mods = modlister();
+
+            string args = "";
+            if (ConfigManager.fetch("ArmA2", "window") == "true")
+            {
+                args += " -window";
+            }
+            if (ConfigManager.fetch("ArmA2", "nosplash") == "true")
+            {
+                args += " -nosplash";
+            }
+            if (ConfigManager.fetch("ArmA2", "skipIntro") == "true")
+            {
+                args += " -skipIntro";
+            }
+            if (ConfigManager.fetch("ArmA2", "noLogs") == "true")
+            {
+                args += " -noLogs";
+            }
+            if (ConfigManager.fetch("ArmA2", "noPause") == "true")
+            {
+                args += " -noPause";
+            }
+            if (ConfigManager.fetch("ArmA2", "showScriptErrors") == "true")
+            {
+                args += " -showScriptErrors";
+            }
+            if (connectToServer == true)
+            {
+                if (server == "PA Repo")
+                {
+                    // insert ArmA 2 OA server here!!!! !!! 
+                    args += " -port=2302 -connect=216.155.136.19 -password=PA";
+                }
+            }
+            if (world != "")
+            {
+                if (world == "demwaffels")
+                {
+                    System.Diagnostics.Process.Start("http://www.youtube.com/watch?v=8W5WdS7q3ns");
+                    return;
+                }
+                else
+                {
+                    args += " -world=" + world;
+                }
+            }
+            if (customParams != "")
+            {
+                args += " " + customParams;
+            }
+            if (mods != "")
+            {
+                args += " -mod=" + mods;
+            }
+
+            Process.Start(arma2Path, args);
+            logIt.addData("Launched Arma 2 with " + args);
+        }
+        static private string modlister() {
+            string modlist = "";
+            foreach (Mod item in WindowManager.mainWindow.a3ModList.Items) {
+                if (modlist == "") {
+                    modlist = item.modName + ";";
+                } else {
+                    modlist += item.modName + ";";
+                }
+            }
+            return modlist;
+        }
+    }
+}
