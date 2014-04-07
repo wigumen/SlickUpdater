de7c20b3476dc666d392b0e8ad8d990504e135fb
wigumen <martin.wigum@gmail.com>
2014-04-07 17:03:41 +0200

    Removed bloat libaried and updated gitignore

--- SlickUpdater/a3UpdateManager.cs
+++ SlickUpdater/a3UpdateManager.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using System.Net;
-using System.ComponentModel;
-using SevenZip;
-using System.Threading;
 
 namespace SlickUpdater {
 
